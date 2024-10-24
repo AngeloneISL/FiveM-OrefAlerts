4345f34b4400a1fd2a8db285c1a481b8f9bcbbc7
AngeloneISL <AlonHaziles@gmail.com>
2024-10-24 15:20:26 +0300

    Add files via upload

---
+++ private_missilealerts/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+Config.ImageScreen = true -- מציג גיפים ותמונות אקראיות מעל ההתראות (ניתן למצוא אותן ב-html/images)
+Config.AlertSound = true -- מנגן צליל כאשר יש שינוי בהתראות
+Config.AlarmLength = 45 -- למשך כמה זמן האזעקה תופיע על המסך של השחקן ( שניות )
+Config.AlertSide = "right" -- left/right (באיזה צד יופיעו ההתראות)
+Config.AnimationTime = 1000 -- כמה זמן לוקח להתראה להופיע או להיעלם
+Config.AlertScanTime = 4 -- כל כמה זמן אנחנו בודקים התראות טילים חדשות, לא מומלץ להוריד מתחת ל-4 (4 שניות), ייתכן שתיחסמו.
---
+++ private_missilealerts/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Original Idea by barbaroNN, Re-Programmed by Angelone'
+description 'Notifys your players about incoming missile attacks on israel'
+version '1.0.0'
+
+lua54 'yes'
+
+server_script 'server.lua'
+client_script 'client.lua'
+
+shared_scripts({
+    'config.lua',
+})
+
+ui_page "html/index.html"
+files{
+    'html/images/*.png',
+    'html/images/*.gif',
+    'html/style.css',
+    'html/sounds/alert.ogg',
+    'html/index.html',
+    'html/script.js',
+}
